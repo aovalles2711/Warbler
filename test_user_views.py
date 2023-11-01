b6c34c07816806c0672206b53ec2ed00e9efb388
aovalles2711 <124206685+aovalles2711@users.noreply.github.com>
2023-11-01 16:31:04 -0500

    Update test_user_views.py

--- test_user_views.py
+++ test_user_views.py
@@ -1,10 +1,15 @@
+"""User View tests."""
+
+# run these tests like:
+#
+#    FLASK_ENV=production python -m unittest test_message_views.py
+
+
 import os
 from unittest import TestCase
 
-#    FLASK_ENV=production python -m unittest test_message_views.py
-
-from models import db,
-connect_db, Message, User, Likes, Follows from bs4 import BeautifulSoup
+from models import db, connect_db, Message, User, Likes, Follows
+from bs4 import BeautifulSoup
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -13,11 +18,21 @@ connect_db, Message, User, Likes, Follows from bs4 import BeautifulSoup
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
+
+# Now we can import app
+
 from app import app, CURR_USER_KEY
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
 
 db.create_all()
 
+# Don't have WTForms use CSRF at all, since it's a pain to test
+
 app.config['WTF_CSRF_ENABLED'] = False
+
 
 class MessageViewTestCase(TestCase):
     """Test views for messages."""
@@ -30,40 +45,40 @@ class MessageViewTestCase(TestCase):
 
         self.client = app.test_client()
 
-        self.testuser = User.signup(username="testuser", 
-                                    email="test@test.com", 
-                                    password="testuser", 
+        self.testuser = User.signup(username="testuser",
+                                    email="test@test.com",
+                                    password="testuser",
                                     image_url=None)
         self.testuser_id = 8989
-        self.testuser_id = self.testuser_id
+        self.testuser.id = self.testuser_id
 
         self.u1 = User.signup("abc", "test1@test.com", "password", None)
-        self.u1_ud = 778
+        self.u1_id = 778
         self.u1.id = self.u1_id
         self.u2 = User.signup("efg", "test2@test.com", "password", None)
         self.u2_id = 884
         self.u2.id = self.u2_id
         self.u3 = User.signup("hij", "test3@test.com", "password", None)
-        self.u4 = User.signup("testing", "test4@test.com", "passwprd", None)
+        self.u4 = User.signup("testing", "test4@test.com", "password", None)
 
         db.session.commit()
-    
+
     def tearDown(self):
         resp = super().tearDown()
-        db.sessio.rollback()
+        db.session.rollback()
         return resp
-    
+
     def test_users_index(self):
         with self.client as c:
             resp = c.get("/users")
 
             self.assertIn("@testuser", str(resp.data))
             self.assertIn("@abc", str(resp.data))
-            self.assertIn("@efg", str(resp.data)), str (resp.data)
+            self.assertIn("@efg", str(resp.data))
             self.assertIn("@hij", str(resp.data))
             self.assertIn("@testing", str(resp.data))
 
-   def test_users_search(self):
+    def test_users_search(self):
         with self.client as c:
             resp = c.get("/users?q=test")
 
